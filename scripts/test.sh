02fc23c9852b2515f22e64cb781823f3564f53e2
Ryan Hirsch <ryan.hirsch@gmail.com>
2019-01-08 16:31:04 -0600

    working

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ set -e
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 PROJ_DIR=$( cd "${SCRIPTS_DIR}" && cd .. && pwd )
 
-cd "${PROJ_DIR}" && yarn test
+cd "${PROJ_DIR}" && yarn test -o

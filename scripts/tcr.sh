11ce292382cbd0ec5bb17ee9ce284f465a2a790e
Ryan Hirsch <ryan.hirsch@gmail.com>
2019-01-07 18:32:08 -0600

    working

--- scripts/tcr.sh
+++ scripts/tcr.sh
@@ -5,6 +5,6 @@ set -e
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 "${SCRIPTS_DIR}/build.sh" \
-  && "${SCRIPTS_DIR}/test.sh" \
-  && "${SCRIPTS_DIR}/commit.sh" \
-  || git reset --hard
+  && ( "${SCRIPTS_DIR}/test.sh" \
+    && "${SCRIPTS_DIR}/commit.sh" \
+    || git reset --hard )

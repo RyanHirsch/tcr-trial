f2506a8fce3a55d5220df4f4f55d9704e28f81f4
Ryan Hirsch <ryan.hirsch@gmail.com>
2019-01-07 18:52:14 -0600

    working

--- scripts/pull-push.sh
+++ scripts/pull-push.sh
@@ -1,3 +1,12 @@
+#! /usr/bin/env bash
+
+set -e
+
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PROJ_DIR=$( cd "${SCRIPTS_DIR}" && cd .. && pwd )
+
+cd "${PROJ_DIR}"
+
 while(true);
 do
   git pull -- rebase;
